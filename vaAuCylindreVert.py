6ff0a652c737335b98aa0192cbf0055052d115d9
Lazarius2160 <76939787+Lazarius2160@users.noreply.github.com>
2021-04-08 23:18:55 +0200

    meme erreur

--- vaAuCylindreVert.py
+++ vaAuCylindreVert.py
@@ -1054,7 +1054,8 @@ if bSTARTUP_TEST:
 # # recherche et rencontre du cylindre
 # recherche et rencontre du cylindre vert : 
 BourineCylindre2(15.0, 1.0, 1.0, True)     # on s'approche a 1 m
-SetBaseMotorsVelocities(siID, iLeftMotor, 0, iRightMotor, 0 )
+SetBaseMotorsVelocities(gsiID, iLeftMotor, 0, iRightMotor, 0 )
+print("cest oooook")
 # puis recherche et rencontre du cylindre rouge :
 #BourineCylindre2(15.0, 1.0, 1.0, False)
 #SetBaseMotorsVelocities(siID, iLeftMotor, 0, iRightMotor, 0 )
@@ -1083,5 +1084,6 @@ SetBaseMotorsVelocities(siID, iLeftMotor, 0, iRightMotor, 0 )
 #..............................
 # deconnexion du serveur V-REP 
 #..............................
-sim.simxFinish(siID)
+sim.simxFinish(gsiID)
 print("deconnexion du serveur.")
+
